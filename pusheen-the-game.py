9431cde0d1a5130376fcc8f802dce9ba19ea9502
Denny <denlee973@users.noreply.github.com>
2017-09-20 22:38:01 -0400

    starting boundaries, gravity not applied
    Should I keep gravity off to make sure boundaries are correct?

--- pusheen-the-game.py
+++ pusheen-the-game.py
@@ -11,7 +11,7 @@ pygame.init()
 
 # Screen Dimensions
 WIDTH = 1280
-HEIGHT= 680
+HEIGHT= 700
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Pusheen the Game')
 
@@ -20,8 +20,8 @@ GREEN = (0,255,0)
 GRASSGREEN = (31,155,70)
 RED = (255,0,0)
 PINK = (255, 183, 237)
-BLUE = (0, 0, 255)
-SKYBLUE = (119,169,249)
+BLUE = (119,169,249)
+SKYBLUE = (206, 225, 255)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
@@ -30,16 +30,32 @@ font = pygame.font.SysFont('Comic Sans MS',20,False,False)
 tfont = pygame.font.SysFont('Comic Sans MS',60,True,False)
 
 # Images
-pusheen1 = pygame.image.load('pusheenWalk-1.png')
-pusheen2 = pygame.image.load('pusheenWalk-2.png')
-pusheen3 = pygame.image.load('pusheenWalk-3.png')
-pusheen4 = pygame.image.load('pusheenWalk-4.png')
+pusheen1L = pygame.image.load('pusheenWalk-1L.png')
+pusheen2L = pygame.image.load('pusheenWalk-2L.png')
+pusheen3L = pygame.image.load('pusheenWalk-3L.png')
+pusheen4L = pygame.image.load('pusheenWalk-4L.png')
+pusheen1R = pygame.image.load('pusheenWalk-1R.png')
+pusheen2R = pygame.image.load('pusheenWalk-2R.png')
+pusheen3R = pygame.image.load('pusheenWalk-3R.png')
+pusheen4R = pygame.image.load('pusheenWalk-4R.png')
+
 table = pygame.image.load('table.png')
 tree = pygame.image.load('tree.png')
 frame = pygame.image.load('frame.jpg')
+cupboard = pygame.image.load('cupboard.png')
 
+# Other settings
+scrn = 2
+pusheenDir = "R"
+pusheenLoc = [0,580]
+pusheenNum = 1
+pusheenNow = pusheen1R
 
+def rect(colour,x,y,w,h):
+    pygame.draw.rect(screen,colour,(x,y,w,h),0)
+# function header
 # Blits background for multiple screens
+
 def background():
     screen.fill(SKYBLUE)
     # Title Screen
@@ -50,10 +66,30 @@ def background():
         pass
     # Gameplay
     elif scrn == 2:
-        pass
+        # Walls
+        rect(WHITE,0,0,32,680)
+        rect(WHITE,1216,0,64,680)
+        # Floor
+        rect(BLUE,0,620,WIDTH,100)
+        # Furniture
+        screen.blit(table,(256,540))
+        screen.blit(tree,(736,450))
+        screen.blit(frame,(620,400))
+        screen.blit(cupboard,(700,120))
+        rect(WHITE,256,543,236,10)
+
     # Score Screen
     elif scrn == 3:
         pass
+
+def boundaries():
+    if pusheenLoc[0] > 450 and pusheenLoc[0] < 550:
+        pass
+    if pusheenLoc[1] < 600:
+        pusheenLoc[1] += 3
+
+def character():
+    screen.blit(pusheenNow,(pusheenLoc[0],pusheenLoc[1]))
 
 def donut():
      
@@ -68,6 +104,7 @@ def donut():
 # Redraws the screen
 def redraw_screen():
     background()
+    character()
     pygame.display.update()
 
 
@@ -84,12 +121,79 @@ while inPlay:
     if keys[pygame.K_ESCAPE]:
         inPlay = False
         
+    
+    if screen == 0:
+        if keys[pygame.K_RETURN]:
+            screen = 1
+            pusheenLoc = [0,580]
 
-     
+
+    if keys[pygame.K_RIGHT]:
+        if pusheenLoc[0] <= WIDTH-35:
+            pusheenLoc[0] += 2
+        while keys[pygame.K_RIGHT]:
+            if pusheenNum <= 10:
+                pusheenNow = pusheen2R
+        
+            elif pusheenNum > 10 and pusheenNum <= 20:
+                pusheenNow = pusheen3R
+                
+            elif pusheenNum > 20 and pusheenNum <= 30:
+                pusheenNow = pusheen4R
+                
+            elif pusheenNum > 30 and pusheenNum <= 40:
+                pusheenNow = pusheen1R
+                
+            if pusheenNum <= 40:
+                pusheenNum += 1
+            else:
+                pusheenNum = 1
+            break
+        pusheenDir = "R"
+    if keys[pygame.K_LEFT]:
+        if pusheenLoc[0] >= 0:
+            pusheenLoc[0] += -2
+        while keys[pygame.K_LEFT]:
+            if pusheenNum <= 10:
+                pusheenNow = pusheen2L
+                
+            elif pusheenNum > 10 and pusheenNum <= 20:
+                pusheenNow = pusheen3L
+                
+            elif pusheenNum > 20 and pusheenNum <= 30:
+                pusheenNow = pusheen4L
+                
+            elif pusheenNum > 30 and pusheenNum <= 40:
+                pusheenNow = pusheen1L
+                
+            if pusheenNum <= 40:
+                pusheenNum += 1
+            else:
+                pusheenNum = 1
+            break
+        pusheenDir = "L"
+    if keys[pygame.K_UP]:
+        if pusheenLoc[1] >= 550:
+            if keys[pygame.K_LEFT]:
+                while pusheenLoc[1] >= 400:
+                    pusheenLoc[1] += -2
+                    pusheenLoc[0] += -1
+                    redraw_screen()
+            elif keys[pygame.K_RIGHT]:
+                while pusheenLoc[1] >= 400:
+                    pusheenLoc[1] += -2
+                    pusheenLoc[0] += 1
+                    redraw_screen()
+            else:
+                while pusheenLoc[1] >= 400:
+                    pusheenLoc[1] += -2
+                    redraw_screen()
+
+
 
     #global variables for position values
     redraw_screen()                     # the screen window must be constantly redrawn - animation
-    pygame.time.delay(2)                # pause for 2 miliseconds
+    pygame.time.delay(4)                # pause for 2 miliseconds
 #---------------------------------------#                                        
 pygame.quit()                           # always quit pygame when done!
 
